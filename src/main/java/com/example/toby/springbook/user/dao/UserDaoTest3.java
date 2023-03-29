c266dfc0e8175ee9ac7458daf0cdd092edb156f2
MoonJaeSeung <wotmdskfk32@naver.com>
2023-03-29 10:29:58 +0900

    getCount() 메소드

--- src/main/java/com/example/toby/springbook/user/dao/UserDaoTest3.java
+++ src/main/java/com/example/toby/springbook/user/dao/UserDaoTest3.java
@@ -16,11 +16,17 @@ public class UserDaoTest3 {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao",UserDao.class);
 
+        dao.deleteAll();
+        assertThat(dao.getCount(),is(0));
+
         User user = new User();
         user.setId("gyumee");
         user.setName("박성철");
         user.setPassword("springno1");
+
         dao.add(user);
+        assertThat(dao.getCount(),is(1));
+
 
         User user2 = dao.get(user.getId());
 
@@ -29,5 +35,27 @@ public class UserDaoTest3 {
 
         System.out.println(System.getProperty("java.class.path"));
 
+    }
+
+    @Test
+    public void count() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao dao = context.getBean("userDao",UserDao.class);
+
+        User user1 = new User("JS","문재승","no1");
+        User user2 = new User("YH","김윤한","no2");
+        User user3 = new User("SJ","황석준","no3");
+
+        dao.deleteAll();
+        assertThat(dao.getCount(),is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(),is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(),is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(),is(3));
     }
 }
